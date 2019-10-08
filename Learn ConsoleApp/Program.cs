e00240c0b06f35dfe28558bc4c7da267e8ce11fa
Luís Dionísio Fernandes <luis.fernandes@hhs.pt>
2019-10-08 12:56:00 +0100

    Add project files.

---
+++ Learn ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O que disse ele?");
+            string disse = Console.ReadLine();
+            Console.WriteLine("Ele Disse: " + disse+ " Caralhes");
+            Console.ReadLine();
+
+        }
+    }
+}
