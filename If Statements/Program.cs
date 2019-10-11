4ba1c26c85313c6eb39ee0f572d10d5bb8cc88b9
Luís Dionísio Fernandes <luis.fernandes@hhs.pt>
2019-10-11 13:18:04 +0100

    if statements done

--- If Statements/Program.cs
+++ If Statements/Program.cs
@@ -10,6 +10,33 @@ namespace If_Statements
     {
         static void Main(string[] args)
         {
+           Console.WriteLine( GetMax(20,10,40));
+            
+            Console.ReadLine();
+
         }
-    }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+                
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+                                 
+            return result;
+
+        
+        }
+
+}
 }
