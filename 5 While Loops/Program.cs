e122695e1b5e93d56b545fd8ed0de5fbeb686237
Luis Dionisio Fernandes <luiscdf@outlook.pt>
2019-10-13 00:17:49 +0100

    6

--- 5 While Loops/Program.cs
+++ 5 While Loops/Program.cs
@@ -10,6 +10,30 @@ namespace _5_While_Loops
     {
         static void Main(string[] args)
         {
+
+            //While Valida a contição e enquanto esta for verdade, executa o codigo dentro das {}
+
+            int index = 1;
+            while (index <=500)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.ReadLine();
+
+            //Do while executa o codigo dentro das {} antes de validar a condição
+
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 600);
+             Console.ReadLine();
+
+
+
+
+
         }
     }
 }
---
+++ 6 Building a Guessing Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Building_a_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretWord = "Maria";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess!=secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Quem é a moçinha mais linda do mundo?? ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true; // bool flag
+                }
+            }
+
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("Já te foste!");
+            }
+            else
+            {
+                Console.Write("Boa, acertaste no nome da menina!");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
