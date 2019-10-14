a38fc6c95a57df6d98d2ab6853197eca779e3cb9
Luís Dionísio Fernandes <luis.fernandes@hhs.pt>
2019-10-14 12:52:50 +0100

    aulas até 9

---
+++ 8 Building an Exponent Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Building_an_Exponent_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(3, 2));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int basenum, int pownum)
+        {
+            int result =1;
+
+            for (int i=0; i<pownum; i++) // loop quantas vezes quanto o powNum
+            {
+                result = result * basenum;
+            }
+
+            return result;
+        }
+
+    }
+}
