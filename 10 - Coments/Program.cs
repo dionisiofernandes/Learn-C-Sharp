418a13c6769e4ea9db91b134e1524dc61189403c
Luís Dionísio Fernandes <luis.fernandes@hhs.pt>
2019-10-14 13:11:36 +0100

    11

---
+++ 10 - Coments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Coments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comment Simple line
+
+            /*
+            Console.ReadLine(); //multiple lines comments
+            */
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11 Exception Hanling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Exception_Hanling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter another nmber: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Mas tás parvo?? " + e.Message);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally // é sempre executado mesmo apesar de erros
+            {
+                Console.WriteLine("Esta Terminou");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
