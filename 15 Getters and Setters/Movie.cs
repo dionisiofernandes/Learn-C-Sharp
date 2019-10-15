6471849e70695442adf10ddc89f102a9a921e78d
Luís Dionísio Fernandes <luis.fernandes@hhs.pt>
2019-10-15 13:01:14 +0100

    15

--- 15 Getters and Setters/Movie.cs
+++ 15 Getters and Setters/Movie.cs
@@ -8,7 +8,34 @@ namespace _15_Getters_and_Setters
 {
     class Movie
     {
+        public string title;
+        public string director;
+        private string rating; //podem ser acedidos apenas pelo codigo dentro da classe Movies
 
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+
+
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            
+            }
+        }
 
     }
 }
--- 15 Getters and Setters/Program.cs
+++ 15 Getters and Setters/Program.cs
@@ -10,6 +10,11 @@ namespace _15_Getters_and_Setters
     {
         static void Main(string[] args)
         {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG");
+            Movie shreck = new Movie("Shrek", "Adam Adamson", "PG");
+            Console.WriteLine(avengers.Rating);
+
+            Console.ReadLine();
         }
     }
 }
